9efea6668dc9e23f2d27cd8e2aab0e2ad0c14d4e
Lexi <105933477+lavaya-s@users.noreply.github.com>
2023-01-07 22:11:52 -0500

    Update Block.py

--- Block.py
+++ Block.py
@@ -1,9 +1,7 @@
 import pygame
-
 class Block(pygame.sprite.Sprite):
     def __init__(self,color,width,height):
         pygame.sprite.Sprite.__init__(self)
         self.image=pygame.Surface([10,10])
         self.image.fill(color)
         self.rect=self.image.get_rect()
-        
